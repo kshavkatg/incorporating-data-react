c4e0782481c394e1a0f07800b6b6d4b683577362
Shavkat Kasymov <kshavkatg@gmail.com>
2021-04-24 11:04:00 +0600

    memorizing values

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect, useCallback, useMemo} from 'react'
 
 export function useFetch(uri) {
   const [data, setData] = useState();
@@ -24,15 +24,17 @@ export function useFetch(uri) {
 export function useIterator(items = [], initialIndex = 0) {
   const [i, setIndex] = useState(initialIndex)
 
-  const prev = () => {
+  const prev = useCallback(() => {
     if(i === 0) return setIndex(items.length - 1)
     setIndex(i - 1)
-  }
+  }, [i])
 
-  const next = () => {
+  const next = useCallback(() => {
     if(i === items.length - 1) return setIndex(0)
     setIndex(i + 1)
-  }
+  }, [i])
 
-  return [items[i], prev, next];
+  const item = useMemo(() => item[i], [i])
+
+  return [item || items[i], prev, next];
 }
