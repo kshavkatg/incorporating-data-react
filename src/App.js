f760b747c1d864edd71938ce10808b3baa497d8a
Shavkat Kasymov <kshavkatg@gmail.com>
2021-04-26 15:16:54 +0600

    get the data through the graphql-request

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import GitHubUser from './GitHubUser'
 import {UserRepositories} from './UserRepositories'
 import RepositoryReadme from './RepositoryReadme'
 import SearchForm from './SearchForm'
+import { GraphQLClient } from 'graphql-request'
 
+const query = `
+  query findRepos($login: String!) {
+    user(login: $login) {
+      login
+      name
+      location
+      avatar_url: avatarUrl
+      repositories(first: 100) {
+        totalCount
+        nodes {
+          name
+        }
+      }
+    }
+  }
+`
+const client = new GraphQLClient(
+  "https://api.github.com/graphql",
+  {
+    headers: {
+      Authorization: "token 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"
+    }
+  }
+)
+
+client
+  .request(query, {login: "moontahoe"})
+  .then(result => JSON.stringify(result, null, 2))
+  .then(console.log)
+  .catch(console.error)
+
+
+  
 export default function App() {
   const [login, setLogin] = useState()
   const [repo, setRepo] = useState()
